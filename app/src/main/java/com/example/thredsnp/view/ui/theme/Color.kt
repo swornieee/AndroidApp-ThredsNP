07874778236f95a078e447236f56f62c66a65ab1
swornieee <swornimchaudhary08@gmail.com>
2026-03-03 16:26:08 +0545

    UI: Updated app theme to Maroon Red and disabled dynamic coloring

--- app/src/main/java/com/example/thredsnp/view/ui/theme/Color.kt
+++ app/src/main/java/com/example/thredsnp/view/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.thredsnp.view.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val MaroonRed = Color(0xFF800000)
+val MaroonRedLight = Color(0xFFB03030)
+val MaroonRedDark = Color(0xFF4D0000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Grey = Color(0xFF808080)
+val LightGrey = Color(0xFFD3D3D3)
